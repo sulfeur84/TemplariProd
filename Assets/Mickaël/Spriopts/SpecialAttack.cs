b2c1ccefea905da5c0f83da2cafec72b208986c9
Mickaël <ocuspocuslotus@hotmail.com>
2022-01-27 12:40:05 +0100

    Player Script Combine

---
+++ Assets/Mickaël/Spriopts/SpecialAttack.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class SpecialAttack : MonoBehaviour
+{
+    public GameObject specialAttackBox;
+    public GameObject Player;
+    public float AttackDuration;
+    public static bool InAttack;
+    public bool specialCooldown;
+    public Image CooldownImage;
+    public bool OnCooldown;
+    private float timer;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        specialAttackBox.SetActive(false);
+    }
+
+    private void Update()
+    {
+        specialAttackBox.transform.position = Player.transform.position;
+        specialAttackBox.transform.rotation = Player.transform.rotation;
+        if (OnCooldown)
+        {
+            timer += Time.deltaTime;
+            CooldownImage.fillAmount = timer/10;
+            if (timer >= 10)
+            {
+                OnCooldown = false;
+                specialCooldown = false;
+            }
+        }
+    }
+
+    void OnSpecialAttack()
+    {
+        if(InAttack == false && specialCooldown == false)
+        {
+            InAttack = true;
+            specialCooldown = true;
+            specialAttackBox.SetActive(true);
+            timer = 0;
+            OnCooldown = true;
+            Invoke("DestroyBox", AttackDuration);
+        }
+    }
+
+    void DestroyBox()
+    {
+        specialAttackBox.SetActive(false);
+        InAttack = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Debug.LogWarning("j'aimerai un void dead pour détruire l'énnemi pls");
+            GameObject enemy = GameObject.FindWithTag("Enemy");
+        }
+    }
+}
